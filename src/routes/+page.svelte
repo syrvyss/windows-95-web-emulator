30e1273f9200cc12d8c1ea06cac02de37ec51c5f
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-08-17 21:09:14 +0200

    Basic desktop view with taskbar and wallpaper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
+<script lang="ts">
+</script>
 
+<body class="bg-cyan-900">
+    <footer
+        class="flex gap-1 fixed bottom-0 w-full h-10 bg-menu border-t-2 border-menu-highlight p-0.5"
+    >
+        <div
+            class="border-2 active:bg-menu-active active:border-menu-shadow active:border-b-menu-highlight active:border-r-menu-highlight border-menu-highlight border-r-menu-shadow border-b-menu-shadow w-20 p-1"
+        >
+            <img src="" alt="" />
+            <p class="align-baseline text">Start</p>
+        </div>
+
+        <div
+            class="border-l-2 border-r-2 border-l-menu-shadow border-r-menu-highlight"
+        />
+    </footer>
+</body>
