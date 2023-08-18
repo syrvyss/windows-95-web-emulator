6d508446da010f43bce126f0f1394191a33e1a3d
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-08-19 00:54:38 +0200

    Add .drag-none as tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,29 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
+const plugin = require("tailwindcss/plugin");
+
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {
       colors: {
-        "menu": "#9ca3af",
+        menu: "#9ca3af",
         "menu-highlight": "#e5e7eb",
         "menu-shadow": "#4b5563",
-        "menu-active": "#d1d5db"
+        "menu-active": "#d1d5db",
       },
-    }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".drag-none": {
+          "-webkit-user-drag": "none",
+          "-khtml-user-drag": "none",
+          "-moz-user-drag": "none",
+          "-o-user-drag": "none",
+          "user-drag": "none",
+        },
+      });
+    }),
+  ],
 };
