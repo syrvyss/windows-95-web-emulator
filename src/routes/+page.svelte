528e333f85160f6ce9510769a49788b03bc30c16
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-08-18 11:52:01 +0200

    Use windows icon for start button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,20 @@
 <script lang="ts"></script>
 
-<body class="bg-cyan-900">
+<body class="bg-teal-700 font-sans">
+  <section class="p-2 flex flex-col gap-5">
+    {#each programs as item}
+    <DesktopIcon {...item}></DesktopIcon>
+    {/each}
+  </section>
+
   <footer
     class="flex gap-1 fixed bottom-0 w-full h-10 bg-menu border-t-2 border-menu-highlight p-0.5"
   >
     <div
-      class="border-2 active:bg-menu-active active:border-menu-shadow active:border-b-menu-highlight active:border-r-menu-highlight border-menu-highlight border-r-menu-shadow border-b-menu-shadow w-20 p-1"
+      class="flex border-2 active:bg-menu-active active:border-menu-shadow active:border-b-menu-highlight active:border-r-menu-highlight border-menu-highlight border-r-menu-shadow border-b-menu-shadow w-20 p-1"
     >
-      <img src="" alt="" />
-      <p class="align-baseline text">Start</p>
+      <img class="select-none" src="src/assets/desktop_icons/logo.png" alt="" />
+      <p class="align-baseline ml-1 font-bold select-none">Start</p>
     </div>
 
     <div
