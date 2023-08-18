c66dd9c93f58ea0db52d6edc40e425e01156a5f0
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-08-19 00:55:50 +0200

    Add basic open/close functions

--- src/lib/components/DesktopWindow.svelte
+++ src/lib/components/DesktopWindow.svelte
@@ -1,3 +1,19 @@
+<script context="module" lang="ts">
+  let name = "";
+  let icon = "";
+
+  export const open_window = (name: string, icon: string) => {
+    name = name;
+    icon = icon;
+  };
+
+  export const close_window = (name: string) => {
+
+  };
+
+
+</script>
+
 <script lang="ts">
   export let name: string, icon: string;
 </script>
