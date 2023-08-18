915c05478cdc50bb590f77b3b9f2ec9905e44ca8
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-08-18 11:53:28 +0200

    Added desktop icon component with basic styling

---
+++ src/lib/components/DesktopIcon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let name: string, icon: string;
+</script>
+
+<div class="flex flex-col w-20 items-center active:bg-blue-700 cursor-pointer">
+  <img class="w-11" src="{icon}" alt="" />
+  <p
+    class="w-full mt-1 text-xs bg-teal-700 text-center text-white block select-none"
+  >
+    {name}
+  </p>
+</div>
