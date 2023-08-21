b7ea8e96a2e0cf4bd7ad24aa111606a8544692db
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-08-21 13:32:02 +0200

    Add PanelApp component, sends call to openMinimize

---
+++ src/lib/components/PanelApp.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let desktopWindow: { name: string; icon: string; id: string };
+</script>
+
+<a
+  class="h-full btn w-48 flex active:bg-menu-active gap-1 p-1 group"
+  href="/"
+  on:click={() => {
+    dispatch("openMinimize", {
+      id: desktopWindow.id,
+    });
+  }}
+>
+  <img src={desktopWindow.icon} alt="" class="" />
+  <p class="align-middle group-active:font-bold">{desktopWindow.name}</p>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,24 +5,26 @@
 
   import { programs } from "$lib/classes/programs";
 
-    let openedWindows = new Array<{ name: string; icon: string; id: string }>();
+  let openedWindows = new Array<{ name: string; icon: string; id: string }>();
 
   let minimized: boolean;
 
   const handleOpen = (event: CustomEvent) => {
-        openedWindows.push({
-            name: event.detail.name,
-            icon: event.detail.icon,
-            id: crypto.randomUUID(),
-        });
+    openedWindows.push({
+      name: event.detail.name,
+      icon: event.detail.icon,
+      id: crypto.randomUUID(),
+    });
     openedWindows = openedWindows;
   };
 
   const handleClose = (event: CustomEvent) => {
-        openedWindows = openedWindows.filter((e) => {
-            return e.id !== event.detail.id;
+    openedWindows = openedWindows.filter((e) => {
+      return e.id !== event.detail.id;
     });
   };
+
+  const openMinimized = (event: CustomEvent) => {};
 </script>
 
 <body class="bg-teal-700 font-sans">
@@ -47,5 +49,13 @@
     <div
       class="border-l-2 border-r-2 border-l-menu-shadow border-r-menu-highlight"
     />
+
+    {#each openedWindows as item}
+      <PanelApp
+        bind={minimized}
+        on:openMinimize={openMinimized}
+        desktopWindow={item}
+      />
+    {/each}
   </footer>
 </body>
