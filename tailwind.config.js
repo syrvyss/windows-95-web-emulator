5404289c303138bca5a792e0194c83389cea9e1f
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-08-25 15:51:34 +0200

    Update colors, padding and border widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        menu: "#9ca3af",
-        "menu-highlight": "#e5e7eb",
-        "menu-shadow": "#4b5563",
-        "menu-active": "#d1d5db",
+        "menu": "#bebebe",
+        "menu-highlight": "#dfdfdf",
+        "menu-shadow": "#555555",
+        "menu-active": "#ffffff",
+        "background-blue": "#008080",
+        "title-bar-primary": "#000080",
+        "title-bar-secondary": "#808080"
       },
     },
   },
@@ -23,17 +26,23 @@ export default {
           "-o-user-drag": "none",
           "user-drag": "none",
         },
+
+        ".dots": {
+          "background-image": "radial-gradient(white 0.6px, transparent 0.6px), radial-gradient(black 0.6px, transparent 0.6px)",
+          "background-size": "calc(2.6 * 1px) calc(2.6 * 1px)",
+          "background-position": "0 0, calc(10 * 5px) calc(10 * 5px)"
+        }
       });
     }),
 
     plugin(function ({ addComponents }) {
       addComponents({
         ".btn": {
-          "border-width": "2px",
-          "border-color": "#e5e7eb",
-          "border-bottom-color": "#4b5563",
-          "border-right-color": "#4b5563",
-          "background-color": "#9ca3af",
+          "border-width": "1px",
+          "border-color": "#dfdfdf",
+          "border-bottom-color": "#555555",
+          "border-right-color": "#555555",
+          "background-color": "#bebebe",
           "user-select": "none",
           "&:active": {
             "border-top-color": "#4b5563",
@@ -42,11 +51,28 @@ export default {
             "border-right-color": "#e5e6eb",
           },
         },
+        ".btn-strong": {
+          "border-width": "2px",
+          "border-color": "#dfdfdf",
+          "border-bottom-color": "#555555",
+          "border-right-color": "#555555",
+          "background-color": "#bebebe",
+          "user-select": "none",
+          "&:active": {
+            "border-color": "#dfdfdf",
+            "border-top-color": "#4b5563",
+            "border-left-color": "#4b5563",
+            "border-bottom-color": "#e5e6eb",
+            "border-right-color": "#e5e6eb",
+          },
+        },
         ".panel": {
-          "border-color": "#e5e7eb",
-          "border-bottom-color": "#4b5563",
-          "border-right-color": "#4b5563",
-          "background-color": "#9ca3af",
+          "padding": "0.125rem",
+          "border-width": "1px",
+          "border-color": "#ffffff",
+          "border-bottom-color": "#555555",
+          "border-right-color": "#555555",
+          "background-color": "#bebebe",
         },
       });
     }),
