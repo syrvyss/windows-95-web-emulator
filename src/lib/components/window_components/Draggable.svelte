6d75aa1aeea226bd74ce21749d838c7db0fee81b
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-08-31 17:12:00 +0200

    Add Draggable icon method

---
+++ src/lib/components/window_components/Draggable.svelte
@@ -0,0 +1,8 @@
+<div class="flex">
+  <div
+    class="w-[3px] h-[90%] my-auto bg-white border-2 border-title-bar-secondary border-t-menu-highlight border-l-menu-highlight cursor-col-resize"
+  />
+  <div
+    class="w-[3px] h-[90%] my-auto bg-white border-2 border-title-bar-secondary border-t-menu-highlight border-l-menu-highlight cursor-col-resize"
+  />
+</div>
