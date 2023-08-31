bbcbcf962f04000caa5b12e3f2b3d91395145a5f
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-08-31 10:48:53 +0200

    Make WindowType component instead of determining type elsewhere

---
+++ src/lib/components/WindowType.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Internet from "$lib/components/windows/Internet.svelte";
+  import Text from "./windows/Text.svelte";
+  import ContextMenu from "./ContextMenu.svelte";
+  import Content from "./Content.svelte";
+
+  export let type: string;
+</script>
+
+{#if type === "text"}
+  <ContextMenu {type} />
+  <Text>
+    <Content />
+  </Text>
+{/if}
+
+{#if type === "folder"}
+  <ContextMenu {type} />
+{/if}
+
+{#if type === "internet"}
+  <ContextMenu {type} />
+  <Internet>
+    <Content />
+  </Internet>
+{/if}
