7d852d8ac4dc1daf7864cc4e8553e96196114971
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-09-01 11:31:05 +0200

    Fetch commits on main git branch, save in variable and put in document

--- src/lib/components/WindowType.svelte
+++ src/lib/components/WindowType.svelte
@@ -7,11 +7,14 @@
 
   import About from "./internet_sites/About.svelte";
 
+  import { Octokit } from "@octokit/core";
+
   export let type: string;
   export let name: string;
 
   export let size: { x: number; y: number };
 
+  let commitRawText = "";
   let projectRawText = "";
 
   if (type === "internet") {
@@ -23,12 +26,43 @@
     projectRawText = text ?? "";
   }
 
+  $: commitData = async (): Promise<string> => {
+    if (name !== "Commit history") {
+      return "halol";
+    }
+
+    const octokit = new Octokit();
+
+    let fetched = await octokit
+      .request("GET /repos/syrvyss/windows-95-web-emulator/commits", {
+        owner: "OWNER",
+        repo: "REPO",
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      })
+      .then((e) => e.data)
+      .catch(() => "Oops, my code sucks!");
+
+    let tempText = "__main__\n\n";
+
+    fetched.forEach((e: any) => {
+      let string = `author: ${e.commit.author.name}\ndate: ${e.commit.author.date}\nmessage: ${e.commit.message}\n`;
+      tempText += string;
+    });
+
+    return tempText;
+  };
+
+  $: commitData().then((e) => (commitRawText = e));
 </script>
 
 {#if type === "text"}
-  <Text>
-    <ContextMenu slot="contextMenu" {type} />
-  </Text>
+  {#if name === "Commit history"}
+    <Text text={commitRawText}>
+      <ContextMenu slot="contextMenu" {type} />
+    </Text>
+  {/if}
   {#if name === "Project info"}
     <Text text={projectRawText}>
       <ContextMenu slot="contextMenu" {type} />
