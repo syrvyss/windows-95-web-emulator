cfb30767830f3dd04206b327e7dd316826c5e67d
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-09-10 13:13:59 +0200

    Wrap sections in article element with gap for more consistency (#52)

--- src/lib/components/internet_sites/About.svelte
+++ src/lib/components/internet_sites/About.svelte
@@ -30,59 +30,58 @@
     </div>
   </section>
 
-  <section id="myself" class="flex h-80 flex-col">
-    <h2 class="text-center text-xl font-black text-yellow-300 underline">
-      About Myself
-    </h2>
+  <article class="my-5 flex flex-col gap-40">
+    <section id="myself" class="flex flex-col">
+      <h2 class="text-center text-xl font-black text-yellow-300 underline">
+        About Myself
+      </h2>
 
-    <p class="text-center">
-      I am a {new Date().getFullYear() - 2003} year old Software Student at LEGO
-      Education, mostly working within a full-stack environment. I spend a lot of
-      time on personal programming projects, and I am also very passionate about
-      open-source software
-      <span class="text-xs"
-        >(this is also why I've taken the decision to GPL this project).</span
-      >
-    </p>
-  </section>
+      <p class="text-center">
+        I am a {new Date().getFullYear() - 2003} year old Software Student at LEGO
+        Education, mostly working within a full-stack environment. I spend a lot
+        of time on personal programming projects, and I am also very passionate about
+        open-source software
+        <span class="text-xs"
+          >(this is also why I've taken the decision to GPL this project).</span
+        >
+      </p>
+    </section>
 
-  <section id="experience" class="flex h-80 flex-col px-5">
-    <h2 class="text-center text-xl font-black text-yellow-300 underline">
-      Which technologies do I have experience in?
-    </h2>
-    <p class="text-center">
-      I have work experience in programming languages such as TypeScript,
-      JavaScript, and C++, mostly using web technologies such as React. A lot of
-      my personal projects are in lower-level languages such as Rust. I also
-      have additional experience in C# as part of my schooling.
-      <br />
-      Apart from work experience, I have experience in Svelte
-      <span class="text-xs"> (which has been used for this project)</span>,
-      Machine-Learning, Data-Science, Compilers
-      <span class="text-xs"> (and lexers & parsers)</span>, UNIX operating
-      systems, and more.
-    </p>
-  </section>
+    <section id="experience" class="flex flex-col px-5">
+      <h2 class="text-center text-xl font-black text-yellow-300 underline">
+        Which technologies do I have experience in?
+      </h2>
+      <p class="text-center">
+        I have work experience in programming languages such as TypeScript,
+        JavaScript, and C++, mostly using web technologies such as React. A lot
+        of my personal projects are in lower-level languages such as Rust. I
+        also have additional experience in C# as part of my schooling.
+        <br />
+        Apart from work experience, I have experience in Svelte
+        <span class="text-xs"> (which has been used for this project)</span>,
+        Machine-Learning, Data-Science, Compilers
+        <span class="text-xs"> (and lexers & parsers)</span>, UNIX operating
+        systems, and more.
+      </p>
+    </section>
 
-  <section id="interests" class="flex h-80 flex-col px-5">
-    <h2 class="text-center text-xl font-black text-yellow-300 underline">
-      What are you interested in?
-    </h2>
-    <p class="text-center">
-      I am very interested in everything open-source, I try to use as much
-      public software as I can, and I use Linux as my personal operating system.
-      Apart from technology, I like learning more about academic subjects such
-      as History, Culture, and Language. I also read books in my free time <span
-        class="text-xs"
-      >
-        (non-fiction, fantasy, mostly.)
-      </span>
-    </p>
-  </section>
-  <section id="projects" class="flex h-80 flex-col px-5">
-    <h2 class="text-center text-xl font-black text-yellow-300 underline">
-      What are your past projects?
-    </h2>
-    <p class="text-center">I don't have many public repositories, sorry!</p>
-  </section>
+    <section id="interests" class="flex flex-col px-5">
+      <h2 class="text-center text-xl font-black text-yellow-300 underline">
+        What are you interested in?
+      </h2>
+      <p class="text-center">
+        I am very interested in everything open-source, I try to use as much
+        public software as I can, and I use Linux as my personal operating
+        system. Apart from technology, I like learning more about academic
+        subjects such as History, Culture, and Language. I also read books in my
+        free time <span class="text-xs"> (non-fiction, fantasy, mostly.) </span>
+      </p>
+    </section>
+    <section id="projects" class="flex flex-col px-5">
+      <h2 class="text-center text-xl font-black text-yellow-300 underline">
+        What are your past projects?
+      </h2>
+      <p class="text-center">I don't have many public repositories, sorry!</p>
+    </section>
+  </article>
 </article>
