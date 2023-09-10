b85c5565ec1b6159a136080cfabd0ae310dad4d7
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-09-10 13:08:58 +0200

    Simple screen size warning (#51)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,24 @@
-<script>
+<script lang="ts">
   import "../app.css";
+
+  let continued = false;
+  let innerWidth: number;
 </script>
 
-<slot />
+<svelte:window bind:innerWidth />
+
+{#if innerWidth <= 950 && !continued}
+  <div class="flex h-full w-full flex-col items-center bg-black">
+    <p class="pb-4 pt-36 text-xl text-white">
+      This site may not work well on smaller screens.
+    </p>
+    <button
+      class=" w-[90%] rounded-sm bg-white p-1"
+      on:click={() => {
+        continued = true;
+      }}>Continue</button
+    >
+  </div>
+{:else}
+  <slot />
+{/if}
